f696a0fc4fd2abf5a7c419b99079f3afce9aa7a2
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-07-20 22:14:19 +0300

    Updates

---
+++ precache-manifest.1b11eecffbee3f165858300e07e7be80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5214d60d4b45190d864698be2b098d92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "962ab8d08ec7c13594a5",
+    "url": "./static/css/main.144b51f7.chunk.css"
+  },
+  {
+    "revision": "2fe38e2f83f18abc33a9",
+    "url": "./static/js/2.84b116fd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.84b116fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962ab8d08ec7c13594a5",
+    "url": "./static/js/main.8808ac99.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
